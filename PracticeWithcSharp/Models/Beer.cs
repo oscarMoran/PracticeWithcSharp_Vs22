87cc014515e11ee54289c33b8496397a8707ea8c
oscarmoran <oscarmoran@effit.com.mx>
2022-05-19 13:23:03 -0500

    Add project files.

---
+++ PracticeWithcSharp/Models/Beer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeWithcSharp.Models
+{
+    public class Beer
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Size { get; set; }
+        public string AlcoholGrade { get; set; }
+        public string CountryOrigin { get; set; }
+    }
+}
---
+++ PracticeWithcSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PracticeWithcSharp.Models;
+
+List<Beer> listBeers = new() {
+    new Beer {
+        Name = "Leon",
+        Brand = "Modelo",
+        AlcoholGrade = "15%",
+        Size = "grande",
+        CountryOrigin = "Mexico"
+    },
+    new Beer {
+        Name = "Indio",
+        Brand = "Moctezuma",
+        AlcoholGrade = "7,8%",
+        Size = "grande",
+        CountryOrigin = "Mexico"
+    },
+    new Beer {
+        Name = "Tecate",
+        Brand = "Moctezuma",
+        AlcoholGrade = "17%",
+        Size = "grande",
+        CountryOrigin = "Mexico"
+    },
+    new Beer {
+        Name = "Victoria",
+        Brand = "Modelo",
+        AlcoholGrade = "4,5%",
+        Size = "laton",
+        CountryOrigin = "Mexico"
+    },
+    new Beer {
+        Name = "Negra Modelo",
+        Brand = "Modelo",
+        AlcoholGrade = "4,7%",
+        Size = "laton",
+        CountryOrigin = "Mexico"
+    }
+};
+listBeers.ForEach(beer => Console.WriteLine($"{beer.Name} de {beer.Brand} con {beer.AlcoholGrade} grados de alcohol, presentacion {beer.Size} y su pais de origen {beer.CountryOrigin}."));
